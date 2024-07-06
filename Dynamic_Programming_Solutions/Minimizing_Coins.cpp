33b089a957b046c7fd4d02acd8667340449e4ae6
Ayush Maurya <amaurya5556@gmail.com>
2024-07-06 23:53:39 +0530

    dp: minimizing coins

--- Dynamic_Programming_Solutions/Minimizing_Coins.cpp
+++ Dynamic_Programming_Solutions/Minimizing_Coins.cpp
@@ -13,26 +13,44 @@ using pll = pair<ll, ll>;
 #define LONG_INF (ll) 1e18
 #define INF (ll) 1e9 
 
-int helper(vi &arr, int index, int sum, vector<vi> &dp) {
+// int helper(vi &arr, int index, int sum, vector<vi> &dp) {
 
-    if(index >= arr.size()) {
-        if(sum == 0) {
-            return 0;
-        } else {
-            return 1e9;
-        }
+//     if(index >= arr.size()) {
+//         if(sum == 0) {
+//             return 0;
+//         } else {
+//             return 1e9;
+//         }
+//     }
+
+//     if(dp[index][sum] != -1) {
+//         return dp[index][sum];
+//     }
+
+//     int choseSame = 1e9;
+//     if(sum - arr[index] >= 0) {
+//         choseSame = 1 + helper(arr, index, sum - arr[index], dp);
+//     }
+//     int choseDiff = helper(arr, index + 1, sum, dp);
+//     return dp[index][sum] = min(choseDiff, choseSame);
+// }
+
+int helper2(vi &arr, int sum, vector<int> &dp) {
+    if(sum == 0) {
+        return 0;
     }
 
-    if(dp[index][sum] != -1) {
-        return dp[index][sum];
+    if(dp[sum] != -1) {
+        return dp[sum];
     }
 
     int choseSame = 1e9;
-    if(sum - arr[index] >= 0) {
-        choseSame = 1 + helper(arr, index, sum - arr[index], dp);
+    for(int i=0;i<arr.size();i++) {
+        if(sum - arr[i] >= 0) {
+            choseSame = min(choseSame, 1 + helper2(arr, sum - arr[i], dp));
+        }
     }
-    int choseDiff = helper(arr, index + 1, sum, dp);
-    return dp[index][sum] = min(choseDiff, choseSame);
+    return dp[sum] = choseSame;
 }
 
 
@@ -43,11 +61,11 @@ void solve() {
     for(auto &it: arr) {
         cin >> it;
     }   
-    vector<vi> dp(size + 1, vi(sum + 1, 0));
-    for(int i=0;i<=sum;i++) {
-        dp[size][i] = 1e9;
-    }
-    dp[size][0] = 0;
+    // vector<vi> dp(size + 1, vi(sum + 1, 0));
+    // for(int i=0;i<=sum;i++) {
+    //     dp[size][i] = 1e9;
+    // }
+    // dp[size][0] = 0;
 
     // dp[i][j] = minium number of coins required to make sum
     // j using all coins including and after index i
@@ -55,18 +73,22 @@ void solve() {
     // dp[size][0] means if we dont include any index
     // still we could make zero without using any coin
 
-    for(int i=size - 1;i >= 0; i--) {
-        for(int j=0;j<=sum;j++) {
-            int choseSame = 1e9;
-            if(j - arr[i] >= 0) {
-                choseSame = 1 + dp[i][j - arr[i]];
-            }
-            int choseDiff = dp[i+1][j];
-            dp[i][j] = min(choseDiff, choseSame);
-        }
-    }
+    // for(int i=size - 1;i >= 0; i--) {
+    //     for(int j=0;j<=sum;j++) {
+    //         int choseSame = 1e9;
+    //         if(j - arr[i] >= 0) {
+    //             choseSame = 1 + dp[i][j - arr[i]];
+    //         }
+    //         int choseDiff = dp[i+1][j];
+    //         dp[i][j] = min(choseDiff, choseSame);
+    //     }
+    // }
+    // cout << (dp[0][sum] != 1e9 ? dp[0][sum] : -1) << endl; 
 
-    cout << (dp[0][sum] != 1e9 ? dp[0][sum] : -1) << endl; 
+    // dp[sum] means minimum coins required to form the given sum
+    // for each sum we try each coin
+    vector<int> dp(sum + 1, -1);
+    cout << (helper2(arr, sum, dp) == 1e9 ? -1 : helper2(arr, sum, dp)) << endl;
 }
 
 int main() {
